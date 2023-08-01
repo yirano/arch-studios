8955ffa98f1e063045266db1ed9787bdcf0378bb
Yirang <yiranga@analuisa.com>
2023-07-31 21:18:58 -0400

    Image Content component

--- src/components/icons/Line.tsx
+++
@@ -1,7 +0,0 @@
-export default function Line({ color = "#C8CCD8" }) {
-  return (
-    <svg width="1" height="104" viewBox="0 0 1 104" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="1" height="104" fill="#C8CCD8" />
-    </svg>
-  );
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -13,7 +13,9 @@ module.exports = {
         'medium-grey': '#7D828F',
         'light-grey': '#C8CCD8', 
         'very-light-grey': '#EEEFF4',
-        'red': '#DF5656'
+        'red': '#DF5656',
+        'secondary': '#FFFFFF',
+        'primary':'#1B1D23'
       },
       fontFamily: {
         sans: ['var(--font-leagueSpartan)'],
